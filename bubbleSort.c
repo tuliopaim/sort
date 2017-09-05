a11cf15bc4fd00d482da796faf36717473de3ef3
tuliopaim <tutpaim@gmail.com>
2017-09-05 19:08:13 -0300

    Create bubbleSort.c

---
+++ bubbleSort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void bubbleSort (int * arr , int size)
+{
+
+	/* 
+	* Função de ordenação Bubble Sort
+	* @param endereço do array
+	* @param tamanho do array
+	*/
+
+	int temp;
+	int iCount, jCount;	
+
+	printf ("Metodo: Bubble Sort\n");
+
+	for (iCount = 0 ; iCount < size ; iCount++)
+	{
+		for (jCount = 0 ; jCount < size - 1 ; jCount++)
+		{
+			if (arr[jCount] > arr[jCount + 1])
+			{
+			temp = arr[jCount + 1];
+			arr[jCount + 1] = arr[jCount];
+			arr[jCount] = temp;	
+			}
+		}
+	}
+
+	return ;
+}
+
+int main(){
+
+	return 0;
+}
