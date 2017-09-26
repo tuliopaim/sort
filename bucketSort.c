acd0051726ec20739a9fef2f71118af2d720ec61
tuliopaim <tutpaim@gmail.com>
2017-09-26 16:33:41 -0300

    Update bucketSort.c

--- bucketSort.c
+++ bucketSort.c
@@ -1,61 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
-#define S 10000
+#define S 25
 
- typedef struct {
-         int topo;
-         int balde[100];
- }bucket;
-
-void insertionSort (int * arr , int size);
-void bucketSort(int * arr, int size);
-
-int main(){
-
-	int vetor[S], size;
-	int i, j;
-	clock_t c2, c1;
-	long double tempo[20], media=0;
-	
-	srand((unsigned)time(NULL));
-	
-	printf ("Metodo: Bucket Sort\n");
-
-	for (i = 0; i < 20; ++i){
-		for(j=0;j<S;j++){
-			vetor[j] = rand()%1000000;
-			//printf("%d ", vetor[i]);	
-		}//printf("\n");
-		c1=clock(); //PEGA O CLOCK INICIAL DO PROCESSADOR
-		bucketSort(vetor,S);
-		c2=clock(); //CLOCK FINAL
-		tempo[i] = (c2-c1)*1000.0/CLOCKS_PER_SEC;
-		printf("Tempo vetor %d: %Lf\n",i+1,tempo[i]);
-		media+=tempo[i];
-	}
-	media/=20;
-	printf("Media: %LF\n", media);
-	
-	FILE * p;
-	//SALVAR MEDIAS EM ARQUIVO
-	p = fopen("tempo.txt", "a");
-	for(i=0;i<20;i++)
-		fprintf(p, "Tempo Vetor %d: %LF\n", i+1, tempo[i]);
-	fprintf(p, "------------\nMedia: %LF\n", media);
-	fclose(p);
-
-	/*
-	printf("\nVetor Ordenado: \n");
-	for(i=0;i<S;i++) printf("%d ", vetor[i]);
-	printf("\n");
-
-	printf("\nTempo: %fms\n", tempo);
-	*/
-
-	return 0;
-}
 void insertionSort (int * arr , int size)
 {
 
@@ -83,39 +30,74 @@ void insertionSort (int * arr , int size)
 	return ;
 }
 
-void bucketSort(int * arr, int size){
-	bucket b[size];
+void bucketSort(int * arr, int size, int qnt_baldes){
 	int i,j,k;
-	int maior = 0;
-
-	//SETA O TOPO DOS BALDES EM 0 E DESCOBRE O MAIOR
-	for(i=0;i<size;i++){
-		b[i].topo = 0;
-		if(arr[i] > maior) maior = arr[i];
+	int min = arr[0], max = arr[0];
+	int *balde[qnt_baldes], qntNumeroBalde[qnt_baldes], alocadosBalde[qnt_baldes];
+	int tamanhoBalde = 0;
+	for (int i = 0; i < qnt_baldes; ++i){
+		alocadosBalde[i] = 1;
+		qntNumeroBalde[i] = 0;
+		balde[i] = malloc(sizeof(int) * alocadosBalde[i]);
 	}
 
-	//VERIFICA EM QUE BALDE O ELEMENTO FICA
-	for(i=0;i<size;i++){
-		j=size-1;
-		while(1){
-			if(j<0) break;
-			if((arr[i] > j*(maior/size) && (arr[i] <= (j+1)*(maior/size)))){
-				b[j].balde[b[j].topo++] = arr[i];
-				break;
+	for (int i = 0; i < size; ++i){
+		min = min < arr[i] ? min : arr[i];
+		max = max > arr[i] ? max : arr[i];
+	}
+
+	tamanhoBalde = ((max - min) / (float)qnt_baldes) + 0.5f;
+
+	for(int i = 0; i < size; ++i){
+		for (int j = 0; j < qnt_baldes; ++j){
+			if(min + (tamanhoBalde * (j + 1)) - 1 < arr[i] && j + 1 < qnt_baldes) continue;
+			if(alocadosBalde[j] <= qntNumeroBalde[j]){
+				alocadosBalde[j] *= 2;
+				balde[j] = realloc(balde[j], sizeof(int) * alocadosBalde[j]);
 			}
-			j--;
+			balde[j][qntNumeroBalde[j]++] = arr[i];
+			break;
 		}
 	}
 
-	//ORDENAR OS BALDES
-	for(i=0;i<size;i++)
-		insertionSort(b[i].balde, b[i].topo);
 
-	//VOLTAR OS ELEMENTOS PRO VETOR
-	i=0;
-	for(j=0;j<size;j++){
-		for(k=0;k<b[j].topo;k++){
-			arr[i++]=b[j].balde[k];
+	k = 0; // Posição no Array final
+	for (int i = 0; i < qnt_baldes; ++i){
+		insertionSort(balde[i], qntNumeroBalde[i]);
+		for (int j = 0; j < qntNumeroBalde[i]; ++j){
+			arr[k++] = balde[i][j];
 		}
-	}	
+	}
+}
+
+
+int main(){
+
+	int vetor[S], size;
+	int i;
+	clock_t c2, c1;
+	double tempo;	
+
+	srand((unsigned)time(NULL));
+	
+	printf ("Metodo: Bucket Sort\n");
+
+	printf("Vetor: \n");
+	for(i=0;i<S;i++){
+		vetor[i] = rand()%1000;
+		printf("%d ", vetor[i]);	
+	}printf("\n");
+
+	c1=clock();
+	bucketSort(vetor, S, 250);
+	c2=clock();
+	tempo = (c2-c1)*1000.0/CLOCKS_PER_SEC;	
+
+	printf("\nVetor Ordenado: \n");
+	for(i=0;i<S;i++) printf("%d ", vetor[i]);
+	printf("\n");
+
+	printf("\nTempo: %fms\n", tempo);
+
+	return 0;
 }
